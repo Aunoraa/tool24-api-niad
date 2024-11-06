4e643a8b6751fb527c7b1126c53a230dbef0773b
Aunora <lekhactuan1412@gmail.comm>
2024-11-06 15:35:02 +0700

    update

--- api/db.go
+++ api/db.go
@@ -3,39 +3,56 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"os"
 )
 
 type Db struct {
-	conn *pgx.Conn
+	conn *pgxpool.Pool
 }
 
 func NewDb() (*Db, error) {
+	//
 	//err := godotenv.Load()
+	//
 	//if err != nil {
+	//
 	//	log.Fatal("Error loading .env file")
+	//
 	//}
+
 	host := os.Getenv("DB_HOST")
+
 	port := os.Getenv("DB_PORT")
+
 	user := os.Getenv("DB_USER")
+
 	password := os.Getenv("DB_PASSWORD")
+
 	dbName := os.Getenv("DB_NAME")
 
 	if host == "" || port == "" || user == "" || password == "" || dbName == "" {
+
 		return nil, fmt.Errorf("thiếu biến môi trường database")
+
 	}
 
 	connString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, password, host, port, dbName)
 
-	conn, err := pgx.Connect(context.Background(), connString)
+	conn, err := pgxpool.Connect(context.Background(), connString)
+
 	if err != nil {
+
 		return nil, fmt.Errorf("không thể kết nối đến cơ sở dữ liệu: %v", err)
+
 	}
 
 	return &Db{conn: conn}, nil
+
 }
 
-func (db *Db) Close() {
-	db.conn.Close(context.Background())
-}
+//func (db *Db) Close() {
+//
+//	db.conn.Close(context.Background())
+//
+//}
