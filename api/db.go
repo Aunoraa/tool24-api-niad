5665fe448500bb2767ae6f0a22a9c3c2e165968a
Aunora <lekhactuan1412@gmail.comm>
2024-11-07 15:37:53 +0700

    update migration

--- api/db.go
+++ api/db.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/cockroachdb"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"os"
 )
@@ -32,5 +35,38 @@ func NewDb() (*Db, error) {
 	if err != nil {
 		return nil, fmt.Errorf("không thể kết nối đến cơ sở dữ liệu: %v", err)
 	}
+
 	return &Db{conn: conn}, nil
+}
+
+func (db *Db) Migrate() error {
+	connString := fmt.Sprintf("cockroachdb://%s:%s@%s:%s/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+
+	m, err := migrate.New(
+		"file://db/migrations",
+		connString,
+	)
+	if err != nil {
+		return fmt.Errorf("lỗi khi tạo migration: %v", err)
+	}
+
+	// Thực hiện migration
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		// Kiểm tra nếu lỗi là dirty database
+		if err.Error() == "Dirty database version 1. Fix and force version." {
+			fmt.Println("Database đang ở trạng thái dirty, đang thực hiện Force version về 1")
+			if forceErr := m.Force(1); forceErr != nil {
+				return fmt.Errorf("không thể Force version: %v", forceErr)
+			}
+			// Thực hiện lại migration sau khi force
+			if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+				return fmt.Errorf("lỗi khi thực hiện lại migration: %v", err)
+			}
+		} else {
+			return fmt.Errorf("lỗi khi thực hiện migration: %v", err)
+		}
+	}
+
+	fmt.Println("Migration đã hoàn thành!")
+	return nil
 }
